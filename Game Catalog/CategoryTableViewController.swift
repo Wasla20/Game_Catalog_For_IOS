6afa4b4f890e796cc8346cb43046966e04f46d39
Wasla20 <63936700+Wasla20@users.noreply.github.com>
2023-09-28 18:11:54 +0500

    Cells made swappable and delete cell functionality added

--- Game Catalog/CategoryTableViewController.swift
+++ Game Catalog/CategoryTableViewController.swift
@@ -7,17 +7,21 @@
 
 import UIKit
 import CoreData
+import SwipeCellKit
 
 class CategoryTableViewController: UITableViewController {
 
     
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    let catArray = ["Action", "Adventure", "Fantasy"]
+    var catArray = [GameCategory]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.rowHeight = 80.0
+        loadItems()
         tableView.reloadData()
+        
         // Do any additional setup after loading the view.
     }
     @IBAction func addButtoonPressed(_ sender: UIBarButtonItem) {
@@ -25,26 +29,25 @@ class CategoryTableViewController: UITableViewController {
         var textfield = UITextField()
         
         let alert = UIAlertController(title: "Enter A New Category", message: "", preferredStyle: .alert)
-        
-        
-        
-        let newCategory = GameCategory(context : self.context)
-        
-        newCategory.name = textfield.text ?? ""
-        
-        
         let action = UIAlertAction(title: "Create", style: .default){(action) in
-            print("User added category :  \(textfield.text ?? "None")")
             
+            let newCategory = GameCategory(context : self.context)
+            
+            newCategory.name = textfield.text!
+            let random = arc4random_uniform(100) + 1
+            newCategory.numberOfGames = Int16(random)
+            
+            self.saveItems()
             
         }
         
         alert.addTextField{(alertTextField) in
             alertTextField.placeholder = "Create Category"
             textfield = alertTextField
+            
+
         }
         
-       
         
         alert.addAction(action)
         present(alert, animated: true)
@@ -54,13 +57,19 @@ class CategoryTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         catArray.count
     }
+    
+    /*override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SwipeTableViewCell
+        cell.delegate = self
+        return cell
+    }*/
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for : indexPath)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for : indexPath) as! SwipeTableViewCell
+        cell.delegate = self
         let category = catArray[indexPath.row]
-        
-        cell.textLabel?.text = category
+        cell.textLabel?.text = category.name
         return cell
 
     }
@@ -68,8 +77,16 @@ class CategoryTableViewController: UITableViewController {
     
     //delegate methods
     
+    
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(catArray[indexPath.row])
+        
+        
+        print(catArray[indexPath.row].name ?? "")
+        
+        
+        
+        
         
         tableView.deselectRow(at: indexPath, animated: true) //on select the row does not stay selected and color disappers
     }
@@ -77,13 +94,61 @@ class CategoryTableViewController: UITableViewController {
     
     func saveItems() {
           
+        
+        
           do {
-            try context.save()
+              
+         try context.save()
+              print("category saved to Database")
           } catch {
              print("Error saving context \(error)")
           }
-          
+          loadItems()
           self.tableView.reloadData()
       }
+    
+    func loadItems(){
+        let request : NSFetchRequest <GameCategory> = GameCategory.fetchRequest()
+        do{
+            
+            catArray = try context.fetch(request)
+            print("category loaded from Database")
+
+        }
+        catch{
+            print("error while fetching data : \(error)")
+        }
+        tableView.reloadData()
+    }
 }
 
+
+extension CategoryTableViewController :  SwipeTableViewCellDelegate{
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            print("inside extension")
+            //deletes on click cell
+             self.context.delete(self.catArray[indexPath.row])
+             self.catArray.remove(at: indexPath.row)
+            
+            self.saveItems()
+        }
+        
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon")
+        //tableView.reloadData()
+        return [deleteAction]
+    }
+    
+    //delete on full right swipe does not work , need to figure it out
+   /*
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
+    */
+}
